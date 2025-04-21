a9936a89059ab746fc8e4349a508b33fdd23c18a
AdrianaSo123 <ams328@njit.edu>
2025-04-21 13:19:33 -0400

    Embed CSV data directly in JavaScript to ensure availability

---
+++ src/utils/csvData.js
@@ -0,0 +1,383 @@
+/**
+ * This file contains the CSV data directly embedded in the JavaScript code
+ * to ensure it's always available regardless of webpack configuration
+ */
+
+// Georgia Income Data
+export const georgiaIncomeData = `
+"Data table for Figure 4. Age-adjusted trend in hypertension prevalence among adults aged 18 and over, by sex: United States, 1999–2018",,,
+,Percent (standard error),,
+Survey Period ,All,Men ,Women
+1999-2000,47.0 (1.4),51.7 (1.8),42.0 (1.5)
+2001-2002,46.0 (1.3),50.2 (2.1),41.5 (1.2)
+2003-2004,44.9 (1.3),49.1 (1.7),40.3 (1.2)
+2005-2006,43.8 (1.1),48.1 (1.4),39.0 (1.2)
+2007-2008,43.2 (0.9),46.8 (1.0),39.5 (1.1)
+2009-2010,42.1 (1.2),45.5 (1.7),38.4 (1.0)
+2011-2012,43.9 (0.9),47.7 (1.4),39.9 (1.1)
+2013-2014,41.7 (0.8) ,45.2 (1.3),38.2 (1.1)
+2015-2016,45.4 (1.0),48.2 (1.4),42.8 (1.3)
+2017-2018,45.4 (1.1),51.0 (1.4),40.2 (1.4)
+`;
+
+// Hypertension County Data
+export const hypertensionCountyData = `
+County,Hypertension Rate
+Appling,38.2
+Atkinson,37.5
+Bacon,38.4
+Baker,39.1
+Baldwin,36.8
+Banks,35.9
+Barrow,34.7
+Bartow,35.2
+Ben Hill,38.6
+Berrien,37.9
+Bibb,37.3
+Bleckley,38.1
+Brantley,38.4
+Brooks,38.7
+Bryan,33.6
+Bulloch,34.5
+Burke,38.2
+Butts,36.8
+Calhoun,39.3
+Camden,34.2
+Candler,37.8
+Carroll,35.6
+Catoosa,35.4
+Charlton,38.1
+Chatham,35.8
+Chattahoochee,34.9
+Chattooga,37.6
+Cherokee,33.1
+Clarke,33.5
+Clay,39.5
+Clayton,36.2
+Clinch,38.3
+Cobb,32.4
+Coffee,37.9
+Colquitt,37.8
+Columbia,33.8
+Cook,37.8
+Coweta,34.1
+Crawford,37.2
+Crisp,38.5
+Dade,36.3
+Dawson,34.7
+Decatur,38.2
+DeKalb,33.5
+Dodge,38.3
+Dooly,38.7
+Dougherty,38.9
+Douglas,34.8
+Early,39.1
+Echols,37.2
+Effingham,34.6
+Elbert,37.8
+Emanuel,38.1
+Evans,37.9
+Fannin,36.4
+Fayette,33.2
+Floyd,36.5
+Forsyth,31.8
+Franklin,36.7
+Fulton,32.9
+Gilmer,36.1
+Glascock,37.9
+Glynn,36.2
+Gordon,35.8
+Grady,38.1
+Greene,36.9
+Gwinnett,32.1
+Habersham,35.9
+Hall,33.7
+Hancock,39.2
+Haralson,36.4
+Harris,34.7
+Hart,36.8
+Heard,36.7
+Henry,34.2
+Houston,35.1
+Irwin,38.2
+Jackson,35.3
+Jasper,36.7
+Jeff Davis,38.1
+Jefferson,38.4
+Jenkins,38.5
+Johnson,38.2
+Jones,36.4
+Lamar,37.1
+Lanier,37.6
+Laurens,37.6
+Lee,35.2
+Liberty,35.3
+Lincoln,37.5
+Long,36.1
+Lowndes,36.1
+Lumpkin,35.2
+Macon,38.9
+Madison,36.1
+Marion,38.2
+McDuffie,37.6
+McIntosh,37.4
+Meriwether,37.9
+Miller,38.7
+Mitchell,38.6
+Monroe,36.1
+Montgomery,37.9
+Morgan,36.2
+Murray,36.1
+Muscogee,36.5
+Newton,35.4
+Oconee,33.4
+Oglethorpe,36.3
+Paulding,34.1
+Peach,37.1
+Pickens,35.6
+Pierce,38.1
+Pike,36.2
+Polk,36.8
+Pulaski,38.1
+Putnam,36.7
+Quitman,39.4
+Rabun,36.2
+Randolph,39.2
+Richmond,37.1
+Rockdale,35.1
+Schley,37.6
+Screven,38.2
+Seminole,38.8
+Spalding,36.9
+Stephens,36.5
+Stewart,39.1
+Sumter,38.4
+Talbot,38.7
+Taliaferro,38.9
+Tattnall,37.6
+Taylor,38.4
+Telfair,38.5
+Terrell,39.1
+Thomas,37.8
+Tift,37.4
+Toombs,37.8
+Towns,36.3
+Treutlen,38.1
+Troup,36.4
+Turner,38.5
+Twiggs,38.2
+Union,36.1
+Upson,37.5
+Walker,36.2
+Walton,35.1
+Ware,38.1
+Warren,38.7
+Washington,38.2
+Wayne,37.6
+Webster,38.6
+Wheeler,38.3
+White,35.6
+Whitfield,35.4
+Wilcox,38.5
+Wilkes,37.9
+Wilkinson,38.1
+Worth,38.3
+`;
+
+// Hypertension Historical Data
+export const hypertensionHistoricalData = `
+"Data table for Figure 4. Age-adjusted trend in hypertension prevalence among adults aged 18 and over, by sex: United States, 1999–2018",,,
+,Percent (standard error),,
+Survey Period ,All,Men ,Women
+1999-2000,47.0 (1.4),51.7 (1.8),42.0 (1.5)
+2001-2002,46.0 (1.3),50.2 (2.1),41.5 (1.2)
+2003-2004,44.9 (1.3),49.1 (1.7),40.3 (1.2)
+2005-2006,43.8 (1.1),48.1 (1.4),39.0 (1.2)
+2007-2008,43.2 (0.9),46.8 (1.0),39.5 (1.1)
+2009-2010,42.1 (1.2),45.5 (1.7),38.4 (1.0)
+2011-2012,43.9 (0.9),47.7 (1.4),39.9 (1.1)
+2013-2014,41.7 (0.8) ,45.2 (1.3),38.2 (1.1)
+2015-2016,45.4 (1.0),48.2 (1.4),42.8 (1.3)
+2017-2018,45.4 (1.1),51.0 (1.4),40.2 (1.4)
+`;
+
+// Georgia Race Population Data
+export const georgiaRacePopulationData = `
+County,Black Population Percentage
+Appling,18.5
+Atkinson,15.2
+Bacon,14.1
+Baker,47.9
+Baldwin,41.5
+Banks,3.4
+Barrow,13.3
+Bartow,10.3
+Ben Hill,36.2
+Berrien,14.6
+Bibb,54.8
+Bleckley,25.6
+Brantley,5.5
+Brooks,36.8
+Bryan,14.2
+Bulloch,29.4
+Burke,49.5
+Butts,28.3
+Calhoun,61.2
+Camden,24.7
+Candler,25.4
+Carroll,18.2
+Catoosa,3.8
+Charlton,13.4
+Chatham,40.1
+Chattahoochee,28.3
+Chattooga,10.6
+Cherokee,5.7
+Clarke,27.5
+Clay,60.4
+Clayton,69.9
+Clinch,36.8
+Cobb,27.6
+Coffee,28.3
+Colquitt,22.7
+Columbia,16.9
+Cook,29.3
+Coweta,17.3
+Crawford,24.2
+Crisp,43.7
+Dade,1.6
+Dawson,0.5
+Decatur,44.2
+DeKalb,54.8
+Dodge,30.1
+Dooly,49.5
+Dougherty,69.1
+Douglas,44.7
+Early,54.8
+Echols,12.9
+Effingham,17.8
+Elbert,31.0
+Emanuel,33.4
+Evans,33.2
+Fannin,0.8
+Fayette,21.2
+Floyd,14.2
+Forsyth,4.4
+Franklin,6.4
+Fulton,44.5
+Gilmer,1.0
+Glascock,8.5
+Glynn,26.6
+Gordon,3.9
+Grady,27.6
+Greene,38.2
+Gwinnett,27.8
+Habersham,3.9
+Hall,7.4
+Hancock,75.0
+Haralson,6.7
+Harris,17.9
+Hart,12.4
+Heard,13.8
+Henry,37.5
+Houston,33.2
+Irwin,26.0
+Jackson,7.7
+Jasper,24.9
+Jeff Davis,14.0
+Jefferson,54.7
+Jenkins,40.5
+Johnson,36.8
+Jones,25.0
+Lamar,26.3
+Lanier,25.5
+Laurens,40.3
+Lee,16.7
+Liberty,42.2
+Lincoln,33.6
+Long,25.9
+Lowndes,36.8
+Lumpkin,2.0
+Macon,60.3
+Madison,6.3
+Marion,34.4
+McDuffie,38.8
+McIntosh,36.6
+Meriwether,42.0
+Miller,29.6
+Mitchell,47.9
+Monroe,22.3
+Montgomery,17.9
+Morgan,23.6
+Murray,1.6
+Muscogee,46.4
+Newton,43.2
+Oconee,5.5
+Oglethorpe,17.2
+Paulding,24.1
+Peach,45.9
+Pickens,2.1
+Pierce,13.1
+Pike,17.3
+Polk,13.7
+Pulaski,35.8
+Putnam,26.0
+Quitman,46.2
+Rabun,0.6
+Randolph,61.7
+Richmond,56.2
+Rockdale,55.5
+Schley,24.3
+Screven,42.6
+Seminole,34.7
+Spalding,35.9
+Stephens,10.1
+Stewart,61.5
+Sumter,51.8
+Talbot,59.7
+Taliaferro,59.6
+Tattnall,31.4
+Taylor,41.6
+Telfair,39.1
+Terrell,60.7
+Thomas,36.9
+Tift,29.6
+Toombs,26.9
+Towns,0.5
+Treutlen,33.5
+Troup,34.9
+Turner,41.1
+Twiggs,45.3
+Union,0.5
+Upson,30.5
+Walker,4.1
+Walton,15.5
+Ware,25.7
+Warren,61.0
+Washington,52.7
+Wayne,19.7
+Webster,39.9
+Wheeler,38.2
+White,1.7
+Whitfield,3.9
+Wilcox,36.6
+Wilkes,41.0
+Wilkinson,40.7
+Worth,29.9
+`;
+
+// Export a function to get the CSV data by filename
+export function getCSVData(filename) {
+  switch (filename) {
+    case 'GeorgiaIncomeData.csv':
+      return georgiaIncomeData;
+    case 'HypertensionCountyData.csv':
+      return hypertensionCountyData;
+    case 'HypertensionHistoricalData.csv':
+      return hypertensionHistoricalData;
+    case 'georgia race population - Sheet1.csv':
+      return georgiaRacePopulationData;
+    default:
+      console.error(`CSV data not found for: ${filename}`);
+      throw new Error(`CSV data not found for: ${filename}`);
+  }
+}
